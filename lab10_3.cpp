06484f4eb50bea730db1f7c8f8b058368ce0026a
litnps <lalitth21.n@gmail.com>
2025-12-23 15:02:11 +0700

    Accepted : lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,33 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<fstream>
+#include<iomanip>
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    ifstream source;
+
+    source.open("score.txt");
+    string temp;
+    double score, sum = 0, average, SD = 0; 
+    int count = 0;
+
+    while(getline(source, temp)){
+        score = stod(temp);
+        sum += score;
+        SD += score*score;
+
+        ++count;   
+    }
+
+    average = sum/count;
+    SD = sqrt((1.0/count)*SD - average*average);
+
+    cout << setprecision(3) ;
+
+    cout << "Number of data = " << count << "\n";
+    cout << "Mean = " << average << "\n";
+    cout << "Standard deviation = " << SD<< "\n";
+    return 0;
+}
